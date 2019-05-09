b9580a6c98135f1b8d7a5a9b2db35fab61108157
kimdawoon <hec8897@naver.com>
2019-05-09 14:27:39 +0900

    태아보험 랜딩페이지 완성)

---
+++ baby/js/event.js
@@ -0,0 +1,71 @@
+
+    var btn = document.getElementById("close_btn");
+    var banner = document.getElementById("top_banner")
+
+    btn.addEventListener("click",function(){
+        banner.style.display = "none";
+
+    })
+
+    $("#consult_btn").click(function(){
+        var offset = $(".input_area").offset();
+        $("html,body").animate({
+            scrollTop:offset.top
+        },200)
+    })
+
+    $(".consult_btns").click(function(){
+        $(this).css({
+            "background":"#3399CC",
+            "color":"white"
+        });
+        $(".consult_btns").not($(this)).css({
+            "background":"white",
+            "color":"black"
+        })
+    })
+
+    $(".btns1").click(function(){
+        $("#consult_name").val("태아보험 가입")
+    })
+    $(".btns2").click(function(){
+        $("#consult_name").val("가입된 보험 보장분석")
+    })
+    $(".btns3").click(function(){
+        $("#consult_name").val("태아/어린이 보험설계")
+    })
+    $(".btns4").click(function(){
+        $("#consult_name").val("우리가족 보험 리모델링")
+    })
+
+    function submitFn(){
+        var check = document.getElementById("check").checked;
+        var name = document.getElementById("in_name").value;
+        var phone = document.getElementById("in_phone").value;
+        var birth = document.getElementById("in_birth").value;
+        var checkBox = document.getElementById("consult_name").value;
+
+
+
+        if(name == ""){
+            alert("이름을 입력해주세요")
+        }
+        else if(phone == ""){
+            alert("전화번호를 입력해주세요")
+        }
+        else if(birth == ""){
+            alert("생년월일을 입력해주세요")
+        }
+        else if(checkBox ==""){
+            alert("원하시는 상담내용을 선택해주세요")
+        }
+
+        else if(check == false){
+            alert("개인정보 수집 및 이용 동의를 체크해주세요")
+        }
+    
+        else{
+            frm.submit();
+        }
+
+    }
---
+++ baby/result_pro.php
@@ -0,0 +1,43 @@
+<?php
+
+//DB연결
+$conn = mysqli_connect('localhost', 'bmadmins', 'bm1004^^','bmadmins'); 
+
+mysqli_set_charset($conn,"utf8");
+
+$reqName = $_POST['reqName'];
+$reqphone = $_POST['reqPhone'];
+$reqBirth = $_POST['reqBirth'];
+$memo = $_POST['reqMemo'];
+
+$site_code = "태아보험랜딩";
+$time = date('Y-m-d H:i:s');
+
+$sql ="INSERT INTO `tb_consult` (site_code,reqName,reqPhone,reqBirth,reqMemo,Insertdate) 
+VALUES('$site_code','$reqName','$reqphone','$reqBirth','$memo','$time')";
+
+$sql2 ="INSERT INTO `tb_consult_backup` (site_code,reqName,reqPhone,reqBirth,reqMemo,Insertdate) 
+VALUES('$site_code','$reqName','$reqphone','$reqBirth','$memo','$time')";
+
+mysqli_query($conn,$sql);
+mysqli_query($conn,$sql2);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+    alert("신청이 완료되었습니다!")
+    history.back();
+    </script>
+    
+</body>
+</html>
