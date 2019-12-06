f606aa9cb77cc00d019f4073fa342c8bb5995c04
kimdawoon <hec8897@naver.com>
2019-12-06 17:40:09 +0900

    no message

--- jemo1205/js/data.js
+++ jemo1205/js/data.js
@@ -1,87 +1,129 @@
+const AnData = [{
+        no: 0,
+        qa: "1. CMA/MMA 라는 단어를 처음 들어봤다."
+    },
+    {
+        no: 1,
+        qa: "2. 가계부는 단 한번도 써 본적이 없고, 앞으로도 하고 싶지 않다."
+    },
+    {
+        no: 2,
+        qa: "3. 저축성/보장성 보험은 하나도 들지 않았다."
+    },
+    {
+        no: 3,
+        qa: "4. 현금서비스나 카드론은 급할때 가끔 받는다."
+    },
+    {
+        no: 4,
+        qa: "5. 월급이 안 들어오면 당장 다음 달 생활비가 없다."
+    },
+    {
+        no: 5,
+        qa: "6. 돈이 급할때 주로 가족의 도움을 받는다."
+    },
+    {
+        no: 6,
+        qa: "7. 한달 얼마를 지출하는지 파악이 안된다."
+    },
+    {
+        no: 7,
+        qa: "8. 신용카드를 3개 이상 가지고 있다."
+    },
+    {
+        no: 8,
+        qa: "9. 현재 저축하고 있는 상품의 이자율이 기억나지 않는다."
+    },
+    {
+        no: 9,
+        qa: "10.은퇴후 어떻게 살아야 할지 구체적인 계획이 없다."
+    }
+
+]
 String.prototype.replaceAll = function (org, dest) {
     //array fillter prototype
     return this.split(org).join(dest);
 }
-
 var QArendingFn = {
-  
-    DataRender: function () {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                returnJson = JSON.parse(this.responseText)
-
-            }
-        }
-        xhttp.open('POST', 'data/data.json', true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send("data=0")
-
-    }(),
     resultArray: [],
-    QaNAnRender: function (no, mode) {
-        if (mode == "reset") {
-            this.resultPoint = 0
-        }
-        var qaRender = document.getElementById('qa_render');
-        var AnResult = []
-        if (no == returnJson.length) {
-            this.resultRender();
-        } else {
-
-            for (var i = 0; i < returnJson[no].answer.length; i++) {
-                AnResult.push(`<button onclick='QArendingFn.QaNAnRender(${no+1})|QArendingFn.resultArrayPush(${no}|${i})|QArendingFn.pointResult(${no}|${returnJson[no].answer[i].point})'>${returnJson[no].answer[i].qa}</button>`)
-
+    indexNumber: 1,
+    ResultCount: 0,
+    QaNAnRender: function (no) {
+        const QaRender = document.getElementById('qa_render')
+        QaRender.innerHTML = `${AnData[no].qa}`
+    },
+    AnClickData(no, result) {
+        const YesBtn = document.getElementById('yes_btn')
+        const NoBtn = document.getElementById('no_btn')
+        const NumberUl = document.getElementById('num');
+        const Numbers = document.querySelectorAll('.an_number');
+        if (this.indexNumber < 10) {
+            this.indexNumber += 1;
+            if (result == "yes") {
+                this.ResultCount += 1
+                this.resultArray.push(no)
             }
-            var resultArrayHtml = AnResult.toString();
-            var replaceAll1 = resultArrayHtml.replaceAll(',', '');
-            var replaceAll2 = replaceAll1.replaceAll('|', ',');
-
-            qaRender.innerHTML = `  <span class="st_cat step_${no+1}"></span>
-                <h3>${returnJson[no].question}</h3>
-                <div>
-                ${replaceAll2}
-                </div>`
+            this.QaNAnRender(no)
+            for (var i = 0; i < Numbers.length; i++) {
+                Numbers[i].className = 'an_number'
+            }
+            Numbers[no].className = 'an_number active'
+            YesBtn.innerHTML = `<img src="images/qa_yes.png" alt="yes_btn" onclick="QArendingFn.AnClickData(${this.indexNumber},'yes')">`
+            NoBtn.innerHTML = `<img src="images/qa_no.png" alt="no_btn" onclick="QArendingFn.AnClickData(${this.indexNumber},'no')">`
+        } else {
+            if (this.ResultCount >= 3) {
+                NumberUl.style.display = 'none'
+                this.resultRender("B")
+            } else {
+                NumberUl.style.display = 'none'
+                this.resultRender("A")
+            }
         }
 
 
-
     },
-    resultArrayPush: function (no, i) {
-        this.resultArray.push(`${returnJson[no].answer[i].qa}`)
-    },
+    resultArrayPush: function (no, i) {},
     resultPoint: 0,
     pointResult: function (no, point) {
-        this.resultPoint += point
+        // this.resultPoint += point
     },
-    resultRender: function () {
-        var qaRender = document.getElementById('qa_render');
-        if (this.resultPoint >= 20) {
-            qaRender.innerHTML = `<h3 class='result_tit result_good'>재무 상태 좋음</h3>
-                                    <img src='images/result_good.png' alt="결과 아이콘 좋음">
-                                    <p>당신의 재무상태는 좋은 편입니다.</p>
-                                    <p> 자신의 재무상황을 올바르게 파악하고 있고 재테크에 관심이 많아 약간의 전문가 도움을 더하면 돈을 더욱 효과적으로 모을 수 있습니다. 무료재무 상담서비스를 통해 수익률 좋은 재테크 방법을 추천받아 보세요.</p>
-                                    <div class='return_btn return_top' onclick='QArendingFn.QaNAnRender(0,"reset")'>테스트 다시하기</div> <div class='return_btn' onclick='gotoInsert()'>상담 신청하기</div>`
-        } else if (this.resultPoint >= 14) {
-            qaRender.innerHTML = `<h3 class='result_tit result_normal'>재무 상태 보통</h3>
-                                    <img src='images/result_normal.png' alt="결과 아이콘 보통">
-                                    <p>당신의 재무상태는 보통입니다.</p>
-                                    <p> 부족한 분야의 금융지식을 늘리고, 단계별 재무목표 수립을 통해 든든한 노후를 맞이하기 위한 노력이 필요합니다. 무료재무 상담서비스를 통해 든든한 재무설계를 시작해보세요.</p>
-                                    <div class='return_btn return_top' onclick='QArendingFn.QaNAnRender(0,"reset")'>테스트 다시하기</div><div class='return_btn' onclick='gotoInsert()'>상담 신청하기</div>`
+    resultRender: function (result) {
+        const ResultRender = document.getElementById('qa_render_bg');
+        if (result == "A") {
+            ResultRender.innerHTML = `<img src="images/con1_resultA_pc.png" class='pc_bg' alt="">
+                                  <img src="images/con1_resultA_mo_bg.png" class='mo_bg' alt="">
+                                    <div class='test_area'>
+                                        <div class='btn1 btns'>
+                                            <img src="images/consult_btn1.png" alt="">
+                                        </div>
+                                        <div class='btn2 btns'>
+                                            <img src="images/consult_btn2.png" alt="">
+                                        </div>
+                                    </div>
+                                  `
         } else {
-            qaRender.innerHTML = `<h3 class='result_tit result_bad'>재무 상태 나쁨</h3>
-                                    <img src='images/result_bad.png' alt="결과 아이콘 나쁨">
-                                    <p>당신의 재무상태는 나쁨입니다.</p>
-                                    <p> 올바른 소비, 저축, 부채관리는 물론 든든한 재무목표 설정이 시급합니다. 재무전문가를 통해 목표 없는 재테크 투자보다는 건강한 돈관리 습관을 길러보세요. </p>
-                                    <div class='return_btn return_top' onclick='QArendingFn.QaNAnRender(0,"reset")'>테스트 다시하기</div><div class='return_btn' onclick='gotoInsert()'>상담 신청하기</div>`
+            if (result == "B") {
+
+                ResultRender.innerHTML = `<img src="images/con1_resultB_pc.png" class='pc_bg' alt="">
+                                      <img src="images/con1_resultB_mo_bg.png" class='mo_bg' alt="">
+                                      <div class='test_area'>
+                                        <div class='btn1 btns'>
+                                            <img src="images/consult_btn1.png" alt="">
+                                        </div>
+                                        <div class='btn2 btns'>
+                                            <img src="images/consult_btn2.png" alt="">
+                                        </div>
+                                    </div>`
+            }
         }
+
     }
 }
 
-function gotoInsert(){
+function gotoInsert() {
     var FormTag = $('#form').offset()
     $('html,body').animate({
-      scrollTop: FormTag.top
+        scrollTop: FormTag.top
     }, 1000); // }
 }
 
@@ -114,33 +156,25 @@ var dataInserter = {
         }
 
 
-        if(reqName == ""){
+        if (reqName == "") {
             alert('성함을 입력해주세요')
-        }
-        else if(reqBirth ==""){
+        } else if (reqBirth == "") {
             alert('생년월일을 입력해주세요')
-        }
-        else if(reqCheckM == false && reqCheckW == false){
+        } else if (reqCheckM == false && reqCheckW == false) {
             alert('성별을 선택해주세요')
-        }
-        else if(reqPhone ==""){
+        } else if (reqPhone == "") {
             alert('연락처를 입력해주세요')
-        }
-        else if(reqArea ==""){
+        } else if (reqArea == "") {
             alert('지역을 선택해주세요')
-        }
-        else if(reqMemo ==""){
+        } else if (reqMemo == "") {
             alert('상담 분야를 선택하세요')
-        }
-        else if(reqChecked == false){
+        } else if (reqChecked == false) {
             alert('개인정보수집 및 이용에 동의 해주세요')
-        }
-        else{
+        } else {
             var insert = confirm('입력하신 정보로 상담신청 하시겠습니까?')
-          
-            if(insert == true){
-                data = [
-                    {
+
+            if (insert == true) {
+                data = [{
                         TestAnsewer: QArendingFn.resultArray
                     },
                     {
@@ -150,17 +184,16 @@ var dataInserter = {
                         reqArea: reqArea,
                         reqMemo: reqMemo,
                         reqSexflag: reqSexflag,
-                        adget:params.id
+                        adget: params.id
                     }
                 ]
                 var xhttp = new XMLHttpRequest();
                 xhttp.onreadystatechange = function () {
                     if (this.readyState == 4 && this.status == 200) {
                         var returnInsert = JSON.parse(this.responseText)
-                        if(returnInsert.phpresult == 'ok'){
+                        if (returnInsert.phpresult == 'ok') {
                             alert('상담신청이 완료되었습니다:)')
-                        }
-                        else{
+                        } else {
                             alert('상담신청 실패 관리자에게 문의해주세요')
                         }
                     }
@@ -169,12 +202,12 @@ var dataInserter = {
                 xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                 xhttp.send("data=" + JSON.stringify(data))
             }
-            
-        }
-       
-     
-    
 
-      
+        }
+
+
+
+
+
     }
 }
